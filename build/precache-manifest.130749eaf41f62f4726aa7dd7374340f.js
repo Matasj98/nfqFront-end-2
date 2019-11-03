5b4e60d2f83e7863b05d95ce4a5bb1746ca6cce2
Matas <matas.jarilinas@stud.vgtu.lt>
2019-11-03 11:04:19 +0200

    update

---
+++ build/precache-manifest.130749eaf41f62f4726aa7dd7374340f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b1a33025e6617b8eb377ccc0988ec1",
+    "url": "/Matasj98/nfqFront-end-2/index.html"
+  },
+  {
+    "revision": "c1c7683128fbbec4ba41",
+    "url": "/Matasj98/nfqFront-end-2/static/css/main.66974005.chunk.css"
+  },
+  {
+    "revision": "73a6d12f7db5b7ee6cdd",
+    "url": "/Matasj98/nfqFront-end-2/static/js/2.1279f7b7.chunk.js"
+  },
+  {
+    "revision": "c1c7683128fbbec4ba41",
+    "url": "/Matasj98/nfqFront-end-2/static/js/main.54861971.chunk.js"
+  },
+  {
+    "revision": "53380f89db2ea05c1641",
+    "url": "/Matasj98/nfqFront-end-2/static/js/runtime-main.77c59fde.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
